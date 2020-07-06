f55af26083fe5d7d2007673903693d081b97c668
mark <markpackham1@gmail.com>
2020-07-06 14:00:33 +0100

    add Comment, "PHP is magic"

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
+import Comment from "./Comment";
 
 export default class CommentList extends Component{
     render(){
         return(
             <div className="comment-list">
-                Hello world, I am a Comment List.
+                <Comment author="Zsolt">Give me a zsolt hand!</Comment>
+                <Comment author="Colin">How about no.</Comment>
             </div>
         )
 
