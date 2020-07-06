5213da2c41aa83dc2a3a2b55499428383898fad3
mark <markpackham1@gmail.com>
2020-07-06 14:10:35 +0100

    Return nodes in CommentList

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -3,11 +3,18 @@ import Comment from "./Comment";
 
 export default class CommentList extends Component{
     render(){
+const commentNodes = this.props.comments.map(comment => {
+    return(
+    <Comment author={comment.author}>{comment.text}</Comment>
+    )
+})
+
         return(
             <div className="comment-list">
-                <Comment author="Zsolt">Give me a zsolt hand!</Comment>
+                {/* <Comment author="Zsolt">Give me a zsolt hand!</Comment>
                 <Comment author="Colin">How about no.</Comment>
-                <Comment author="Me">PHP is magic!</Comment>
+                <Comment author="Me">PHP is magic!</Comment> */}
+                {commentNodes}
             </div>
         )
 
