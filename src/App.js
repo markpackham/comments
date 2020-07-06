65ea9b50c87cd5b4ece2fed0b634672b6b6cfe7e
mark <markpackham1@gmail.com>
2020-07-06 13:39:47 +0100

    Add CommentBox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
+import CommentBox from "./containers/CommentBox"
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <CommentBox />
   );
 }
 
